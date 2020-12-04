67bee7bb0c39e742effc6056f96719389ee32f91
Sofia Raak <sofia.raak@bristol.ac.uk>
2020-12-04 12:28:04 +0000

    Atttempting to add function to directly download protein FASTA files using entrez Problems with converting downloaded types to appropriate format for parsing: StreamModeError: Fasta files must be opened in text mode

--- molecular_weight.py
+++ molecular_weight.py
@@ -1,3 +1,4 @@
+from Bio import Entrez
 from Bio import SeqIO
 import pandas as pd
 
@@ -7,6 +8,13 @@ fasta_to_weight = {'A':0.0891, 'R':0.1742, 'N': 0.1321, 'D':0.1331, 'C': 0.1212,
                   'L': 0.1312, 'K': 0.1462, 'M': 0.1492, 'F': 0.1652, 'P': 0.1151,
                   'S': 0.1051, 'T': 0.1191, 'W': 0.2042, 'Y': 0.1812, 'V': 0.1171}
 
+#Download sequence files with Entrez
+def DownloadSequence(my_email, acc_numbers, rettype):
+    Entrez.email = my_email
+    handle = Entrez.efetch(db='protein', id=acc_numbers, rettype='text')
+    record = SeqIO(handle, rettype)    
+    return record
+
 #Separate sequences, headers
 def SeparateSequences(file, filetype):
     
@@ -15,7 +23,7 @@ def SeparateSequences(file, filetype):
     
     ID_list = []
     seq_list = []
-    
+        
     for seq_record in SeqIO.parse(file, filetype):
         ID_list.append(seq_record.id)
         seq_list.append(seq_record.seq)
